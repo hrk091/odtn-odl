dd86516c63ca5abc0c38ece7c8e350d39cfd5db0
hiroki <hiroki.okui@gmail.com>
2018-06-16 00:21:43 -0700

    modify tapicommonrpc

--- impl/src/main/java/org/opendaylight/odtn/impl/TapiProvider.java
+++ impl/src/main/java/org/opendaylight/odtn/impl/TapiProvider.java
@@ -7,25 +7,26 @@
  */
 package org.opendaylight.odtn.impl;
 
-import com.google.common.util.concurrent.ListenableFuture;
+import java.util.concurrent.Future;
+
 import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.ProviderContext;
 import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.RpcRegistration;
 import org.opendaylight.controller.sal.binding.api.BindingAwareProvider;
 import org.opendaylight.yang.gen.v1.urn.onf.otcc.yang.tapi.common.rev180307.GetServiceInterfacePointDetailsInput;
 import org.opendaylight.yang.gen.v1.urn.onf.otcc.yang.tapi.common.rev180307.GetServiceInterfacePointDetailsOutput;
 import org.opendaylight.yang.gen.v1.urn.onf.otcc.yang.tapi.common.rev180307.GetServiceInterfacePointDetailsOutputBuilder;
-import org.opendaylight.yang.gen.v1.urn.onf.otcc.yang.tapi.common.rev180307.GetServiceInterfacePointListInput;
 import org.opendaylight.yang.gen.v1.urn.onf.otcc.yang.tapi.common.rev180307.GetServiceInterfacePointListOutput;
 import org.opendaylight.yang.gen.v1.urn.onf.otcc.yang.tapi.common.rev180307.GetServiceInterfacePointListOutputBuilder;
 import org.opendaylight.yang.gen.v1.urn.onf.otcc.yang.tapi.common.rev180307.TapiCommonService;
 import org.opendaylight.yang.gen.v1.urn.onf.otcc.yang.tapi.common.rev180307.UpdateServiceInterfacePointInput;
-import org.opendaylight.yang.gen.v1.urn.onf.otcc.yang.tapi.common.rev180307.UpdateServiceInterfacePointOutput;
-import org.opendaylight.yang.gen.v1.urn.onf.otcc.yang.tapi.common.rev180307.UpdateServiceInterfacePointOutputBuilder;
 import org.opendaylight.yangtools.yang.common.RpcResult;
 import org.opendaylight.yangtools.yang.common.RpcResultBuilder;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 
 public class TapiProvider implements BindingAwareProvider, AutoCloseable,
         TapiCommonService {
@@ -49,8 +50,8 @@ public class TapiProvider implements BindingAwareProvider, AutoCloseable,
     }
 
     @Override
-    public ListenableFuture<RpcResult<GetServiceInterfacePointDetailsOutput>>
-    getServiceInterfacePointDetails(GetServiceInterfacePointDetailsInput input) {
+    public Future<RpcResult<GetServiceInterfacePointDetailsOutput>>
+        getServiceInterfacePointDetails(GetServiceInterfacePointDetailsInput input) {
         LOG.info("********** get-service-interface-point-details called!!");
         GetServiceInterfacePointDetailsOutputBuilder output =
                 new GetServiceInterfacePointDetailsOutputBuilder();
@@ -58,17 +59,16 @@ public class TapiProvider implements BindingAwareProvider, AutoCloseable,
     }
 
     @Override
-    public ListenableFuture<RpcResult<UpdateServiceInterfacePointOutput>>
-    updateServiceInterfacePoint(UpdateServiceInterfacePointInput input) {
+    public Future<RpcResult<Void>>
+        updateServiceInterfacePoint(UpdateServiceInterfacePointInput input) {
         LOG.info("********** update-service-interface-point called!!");
-        UpdateServiceInterfacePointOutputBuilder output =
-                new UpdateServiceInterfacePointOutputBuilder();
-        return RpcResultBuilder.success(output.build()).buildFuture();
+        throw new NotImplementedException();
+//        return RpcResultBuilder.success().buildFuture();
     }
 
     @Override
-    public ListenableFuture<RpcResult<GetServiceInterfacePointListOutput>>
-    getServiceInterfacePointList(GetServiceInterfacePointListInput input) {
+    public Future<RpcResult<GetServiceInterfacePointListOutput>>
+        getServiceInterfacePointList() {
         LOG.info("********** get-service-interface-point-list called!!");
         GetServiceInterfacePointListOutputBuilder output =
                 new GetServiceInterfacePointListOutputBuilder();
